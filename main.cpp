49fc5f8ef4cc298d4b2ca31e047990e57a990e8f
clementnrd <130042480+clementnrd@users.noreply.github.com>
2025-02-07 14:11:10 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,34 @@
+#include <iostream>
+#include "balistique.h" // Inclusion du fichier header
 
+using namespace std;
+
+int main() {
+    double vitesse, angle;
+    char continuer = 'o';
+
+    cout << "=== Simulateur de Tir Balistique ===\n";
+
+    while (continuer == 'o' || continuer == 'O') {
+        cout << "\nEntrez la vitesse initiale du projectile (m/s) : ";
+        cin >> vitesse;
+        cout << "Entrez l'angle de tir (degrés) : ";
+        cin >> angle;
+
+        // Calcul du tir en utilisant la fonction du header
+        ResultatTir resultat = calculerTir(vitesse, angle);
+
+        // Affichage des résultats
+        cout << "\nRésultats du tir balistique :\n";
+        cout << " - Temps de vol : " << resultat.tempsVol << " secondes\n";
+        cout << " - Portée du projectile : " << resultat.portee << " mètres\n";
+        cout << " - Hauteur maximale atteinte : " << resultat.hauteurMax << " mètres\n";
+
+        cout << "\nVoulez-vous faire un autre tir ? (o/n) : ";
+        cin >> continuer;
+    }
+
+    cout << "Merci d'avoir utilisé le simulateur !\n";
+    return 0;
+}
+rt
