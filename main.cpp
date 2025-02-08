ba81d439e548263114e1f6c79b437c8b2f7e45f0
MAWTIN42 <martintailler@gmail.com>
2025-02-08 17:03:21 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,31 +4,31 @@
 using namespace std;
 
 int main() {
-    flaot vitesse_initial, angle ;
-
+    float vitesse_initiale, angle, position_x_lanceur, position_z_lanceur;
+    const int g = 9.81; 
+    const int distance_cible = 2.37 ; 
     cout << "=== Simulateur de Tir Balistique ===\n";
 
-  while (vitesse_initial <= 0){
-        cout << "Entrez la vitesse initiale du projectile (m/s) : " << endl;
-        cin >> vitesse_initial;
-        cout << "Entrez l'angle de tir (degrés) : ";
+while (vitesse_initiale <= 0 || position_x_lanceur <= 0 || position_z_lanceur <=0)
+  {
+        cout << "Entrez la vitesse initiale du projectile (m/s) : "<< endl; 
+        cin >> vitesse_initiale;
+        cout << "Entrez l'angle de tir (degrés) : " << endl ; 
         cin >> angle;
-       }
+        cout << "Entrez la position x du lanceur (m) : " << endl;
+        cin >> position_x_lanceur;
+        cout << "Entrez la position z du lanceur (m) : " << endl ;
+        cin >> position_z_lanceur ; 
+  }
+ 
+    double angle_radian = angle_degre * M_PI / 180.0; 
 
-        // Calcul du tir en utilisant la fonction du header
-        ResultatTir resultat = calculerTir(vitesse_initial, angle);
-
-        // Affichage des résultats
-        cout << "\nRésultats du tir balistique :\n";
-        cout << " - Temps de vol : " << resultat.tempsVol << " secondes\n";
-        cout << " - Portée du projectile : " << resultat.portee << " mètres\n";
-        cout << " - Hauteur maximale atteinte : " << resultat.hauteurMax << " mètres\n";
-
-        cout << "\nVoulez-vous faire un autre tir ? (o/n) : ";
-        cin >> continuer;
-    }
-
-    cout << "Merci d'avoir utilisé le simulateur !\n";
-    return 0;
+    cout << "Temps de vol de la flechette est de (s): " << calculer_temps_de_vol(vitesse_initiale, angle_radian, distance_cible);
+    cout << "Le point d'impactacte de la flechette est de (m):" << calculer_hauteur_impact(position_x_lanceur, position_z_lanceur ,vitesse_initiale, angle_radian, distance_cible); 
+    
+    
+cout << "Merci d'avoir utilisé le simulateur !\n";   
+return 0;
 }
-rt
+
+
