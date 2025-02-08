ecbc0a5ba6c298f73434e9dec9f35e5c82eb94fd
MAWTIN42 <martintailler@gmail.com>
2025-02-08 15:14:20 +0100

    Add files via upload

---
+++ REGLE_LANCER.h
@@ -0,0 +1,23 @@
+#include <iostream>
+
+#ifndef REGLE_LANCER
+#define REGLE_LANCER
+
+
+using namespace std;
+
+void Regle_lancer() {
+    cout << "Bienvenue au jeu de lancer de fléchettes !\n\n";
+    cout << "Règles de base :\n";
+    cout << "1. La distance entre le joueur et la cible est de 2.37 m.\n";
+    cout << "2. La hauteur de la cible est de 1.73 m.\n";
+    cout << "3. Le diametre de la cible est de 0.451 m.\n\n";
+    cout << "Amusez-vous bien et lancez vos fléchettes avec précision !\n";
+}
+
+int main() {
+    Regle_lancer();
+    return 0;
+}
+
+#endif 
