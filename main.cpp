9a41f0d1d53b7afdd647ea62a46041244a03cc25
MAWTIN42 <martintailler@gmail.com>
2025-02-11 13:49:44 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,22 +9,22 @@ int main() {
     const int distance_cible = 2.37 ; 
     cout << "=== Simulateur de Tir Balistique ===\n";
 
-while (vitesse_initiale <= 0 || position_x_lanceur <= 0 || position_z_lanceur <=0)
+do
   {
-        cout << "Entrez la vitesse initiale du projectile (m/s) : "<< endl; 
+        cout << "Entrez la vitesse initiale du projectile (m/s) : "<< endl;
         cin >> vitesse_initiale;
-        cout << "Entrez l'angle de tir (degrés) : " << endl ; 
-        cin >> angle;
+        cout << "Entrez l'angle de tir (degrés) : " << endl ;
+        cin >> angle_degre;
         cout << "Entrez la position x du lanceur (m) : " << endl;
         cin >> position_x_lanceur;
         cout << "Entrez la position z du lanceur (m) : " << endl ;
-        cin >> position_z_lanceur ; 
-  }
+        cin >> position_z_lanceur ;
+  }while(vitesse_initiale <= 0 || position_x_lanceur <= 0 || position_z_lanceur <=0);
  
     double angle_radian = angle_degre * M_PI / 180.0; 
 
     cout << "Temps de vol de la flechette est de (s): " << calculer_temps_de_vol(vitesse_initiale, angle_radian, distance_cible);
-    cout << "Le point d'impactacte de la flechette est de (m):" << calculer_hauteur_impact(position_x_lanceur, position_z_lanceur ,vitesse_initiale, angle_radian, distance_cible); 
+    cout << "Le point d'impactacte de la flechette est de (m):" << calculer_hauteur_impact(position_x_lanceur, position_z_lanceur ,vitesse_initiale, angle_radian, distance_cible, g; 
     
     
 cout << "Merci d'avoir utilisé le simulateur !\n";   
