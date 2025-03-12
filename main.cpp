7218852caa36f24634a0711410491db9a77519c3
MAWTIN42 <martintailler@gmail.com>
2025-03-12 12:04:46 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include "calcul_lancer.h"
 #inculde "regle_lancer.h"
 
+
 using namespace std ;
 int main() {
     // Paramètres du projectile
@@ -11,38 +12,42 @@ int main() {
 
 
     cout << "Entrez la vitesse initiale (m/s) : ";
-    cin >> v0;
-    cout << "Entrez l'angle de tir (degrés) : ";
-    cin >> alpha;
-    cout << "Entrez l'angle du vent (degrés) : ";
-    cin >> phi_y;
+    cin >> v0 ;
+    cout << "Entrez l'angle de tir sur z  (degres) : ";
+    cin >> alpha ;
+    cout << "Entrez l'angle sur y  (degres) : ";
+    cin >> phi_y ;
     cout << "Entrez la position X de la cible (m) : ";
-    cin >> x_cible;
+    cin >> x_cible ;
     cout << "Entrez la position Z de la cible (m) : ";
-    cin >> z_cible;
+    cin >> z_cible ;
     cout << "Entrez la position Y de la cible (m) : ";
-    cin >> y_cible;
+    cin >> y_cible ;
     cout << "Entrez la position position xd du tirreur (m) : ";
-    cin >> xd;
+    cin >> xd ;
     cout << "Entrez la position position zd du tirreur (m) : ";
-    cin >> zd;
+    cin >> zd ;
     cout << "Entrez la position position yd du tirreur (m) : ";
-    cin >> yd;
+    cin >> yd ;
 
 
-    resultat_lancer_z = position_fleche_z(g, x_cible, xd, v0, alpha, phi_y, zd);
+    resultat_lancer_z = position_fleche_z(g, x_cible, xd, v0, alpha, phi_y, zd) ;
 
-    resultat_lancer_y = position_fleche_y(x_cible, xd, phi_y, yd);
+    resultat_lancer_y = position_fleche_y(x_cible, xd, phi_y, yd) ;
 
     temps_vol =  calcul_temps_vol(x_cible, xd, v0, alpha, phi_y) ;
 
+    cout << "Position z " << resultat_lancer_z << endl ;
+    cout << "position y " << resultat_lancer_y << endl ;
 
     if ( resultat_lancer_z < 1.73 || resultat_lancer_z >2.181 || resultat_lancer_y > rayon_cible || resultat_lancer_y < - rayon_cible )
     {
     cout << "Vous n avez pas toucher la cible" ;
     point = 0;
     }
-}   
+
+
+  
 
     return 0;
 }
