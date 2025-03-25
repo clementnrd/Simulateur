0aebd8af44bf5d07d80349f4c94f21e3e5f4fddc
MAWTIN42 <martintailler@gmail.com>
2025-03-25 18:35:20 +0100

    Update REGLE_LANCER.h

--- REGLE_LANCER.h
+++ REGLE_LANCER.h
@@ -7,11 +7,11 @@
 using namespace std;
 
 void Regle_lancer() {
-    cout << "Bienvenue au jeu de lancer de fléchettes !\n\n";
-    cout << "Règles de base :\n";
-    cout << "1. La distance entre le joueur et la cible est de 2.37 m.\n";
-    cout << "2. La hauteur de la cible est de 1.73 m.\n";
-    cout << "3. Le diametre de la cible est de 0.451 m.\n\n";
-    cout << "Amusez-vous bien et lancez vos fléchettes avec précision !\n";
+    cout << "Bienvenue au jeu de lancer de fl"chettes ! << endl;
+    cout << "Regles de base : " <<endl ;
+    cout << "1. La distance entre le joueur et la cible est de 2.37 m." << endl;
+    cout << "2. La hauteur de la cible est de 1.73 m." << endl;
+    cout << "3. Le diametre de la cible est de 0.451 m." << endl;
+    cout << "Amusez-vous bien et lancez vos fléchettes avec précision !" <<endl;
 }
 #endif 
